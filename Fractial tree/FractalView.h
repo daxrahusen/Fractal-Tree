ec36032bdf4e21c9571e21a236b1568402a10d1b
daxrahusen <daxrahusen@mediamonks.com>
2017-10-25 21:08:58 +0200

    initial init

---
+++ Fractial tree/FractalView.h
@@ -0,0 +1,13 @@
+//
+//  FractalView.h
+//  Fractial tree
+//
+//  Created by Dax Rahusen on 16/10/2017.
+//  Copyright © 2017 Dax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FractalView : UIView
+
+@end
---
+++ Fractial tree/FractalView.m
@@ -0,0 +1,21 @@
+//
+//  FractalView.m
+//  Fractial tree
+//
+//  Created by Dax Rahusen on 16/10/2017.
+//  Copyright © 2017 Dax. All rights reserved.
+//
+
+#import "FractalView.h"
+
+@implementation FractalView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
